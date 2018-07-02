371a4af8a965bdb6f9146b399ed624ba26890c17
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-02 00:36:16 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |key, value|
     puts "#{key.capitalize}:"
     value.each do |holiday, supplies|
-      puts "#{holiday.capitalize}: #{supplies.join(',')}"
+      puts "#{holiday.capitalize}: #{supplies.join(', ')}"
     end
   end
 
